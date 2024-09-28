8c71edfbe5e4d5515459a75d54f010e2512311e1
Sabau Daniel Catalin <111538225+Sabau-Daniel@users.noreply.github.com>
2024-09-28 03:34:21 +0300

    Added OOP
    Made the OOP folder with the car and car_dealership files

---
+++ OOP/Car_dealership.py
@@ -0,0 +1,16 @@
+from Car import car
+
+
+car1 = car("Mustang", 2024, "Black", True)
+car2 = car("Logan", 2003, "Blue", True)
+car3 = car("Focus", 2018, "Red", False)
+
+
+
+print(car1.model)
+print(car1.year)
+print(car1.color)
+print(car1.for_sale)
+
+car1.drive()
+car1.stop()
